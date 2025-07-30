9166b5390def89481dc7c7ee95d496ca1e044db8
Vo Quang Chien <voquangchien.dev@proton.me>
2025-07-30 11:40:37 +0700

    feat: add authentication

--- configs/index.cjs
+++ configs/index.cjs
@@ -3,15 +3,20 @@
 const fp = require('fastify-plugin');
 const FastifyEnv = require('@fastify/env');
 
-module.exports = fp(async function configLoader(fastify) {
-  await fastify.register(FastifyEnv, {
-    confKey: 'secrets',
-    schema: fastify.getSchema('schema:dotenv')
-  });
+module.exports = fp(
+  async function configLoader(fastify) {
+    await fastify.register(FastifyEnv, {
+      confKey: 'secrets',
+      schema: fastify.getSchema('schema:dotenv')
+    });
 
-  fastify.decorate('config', {
-    mongo: {
-      url: fastify.secrets.MONGO_URL
-    }
-  });
-});
+    fastify.decorate('config', {
+      mongo: {
+        url: fastify.secrets.MONGO_URL
+      }
+    });
+  },
+  {
+    name: 'application-config'
+  }
+);
---
+++ plugins/auth.cjs
@@ -0,0 +1,57 @@
+const fp = require('fastify-plugin');
+const fastifyJwt = require('@fastify/jwt');
+
+module.exports = fp(
+  async function authenticationPlugin(fastify, _opts) {
+    const revokedTokens = new Map();
+
+    fastify.register(fastifyJwt, {
+      secret: fastify.secrets.JWT_SECRET,
+      trusted: function isTrusted(_request, decodedToken) {
+        return !revokedTokens.has(decodedToken.jti);
+      }
+    });
+
+    fastify.decorate('authenticate', async function authenticate(request, reply) {
+      try {
+        await request.jwtVerify();
+      } catch (err) {
+        reply.send(err);
+      }
+    });
+
+    fastify.post(
+      '/logout',
+      {
+        onRequest: fastify.authenticate
+      },
+      async function logoutHandler(request, reply) {
+        request.revokeToken();
+        reply.code(204);
+      }
+    );
+
+    fastify.decorateRequest('revokeToken', function () {
+      revokedTokens.set(this.user.jti, true);
+    });
+
+    fastify.decorateRequest('generateToken', function () {
+      const token = fastify.jwt.sign(
+        {
+          id: String(this.user._id),
+          username: this.user.username
+        },
+        {
+          jti: String(Date.now()),
+          expiresIn: fastify.secrets.JWT_EXPIRE_IN
+        }
+      );
+
+      return token;
+    });
+  },
+  {
+    name: 'authentication-plugin',
+    dependencies: ['application-config']
+  }
+);
